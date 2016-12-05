58298dfa2e46469a2b9c1a2c00103e0fbe1d3af3
dajoh16 <dajoh16@student.sdu.dk>
2016-12-05 13:43:52 +0100

    Address done

--- OOP1-workshop/src/oop1/workshop/Address.java
+++ OOP1-workshop/src/oop1/workshop/Address.java
@@ -11,4 +11,45 @@ package oop1.workshop;
  */
 public class Address {
 	
+	private final String zipCode;
+	private final String country;
+	private final String streetName;
+	private final int buildingNumber;
+	
+	public Address(String zipCode, String country, String streetName, int buildingNumber){
+		this.buildingNumber = buildingNumber;
+		this.country = country;
+		this.streetName = streetName;
+		this.zipCode = zipCode;
+	}
+
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName() {
+		return streetName;
+	}
+
+	/**
+	 * @return the buildingNumber
+	 */
+	public int getBuildingNumber() {
+		return buildingNumber;
+	}
+	
+	
 }
