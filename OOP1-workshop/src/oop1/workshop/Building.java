040171f4b82ed0e79c5e386a7c8b0d25eee5b18f
dajoh16 <dajoh16@student.sdu.dk>
2016-12-05 13:24:14 +0100

    Oprettelse af klasser

---
+++ OOP1-workshop/src/oop1/workshop/Building.java
@@ -0,0 +1,18 @@
+package oop1.workshop;
+
+import java.util.ArrayList;
+
+public class Building {
+
+    private ArrayList<Sensor> sensors;
+
+    private DBReadings database;
+
+    private Object untitledField;
+
+    private String name;
+
+    private String id;
+
+    private Address address;
+}
---
+++ OOP1-workshop/src/oop1/workshop/DBReadings.java
@@ -0,0 +1,8 @@
+package oop1.workshop;
+
+import java.util.ArrayList;
+
+public class DBReadings {
+
+    private ArrayList<Reading> readings;
+}
